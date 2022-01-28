17137c0958a1ccbab2f78ff497ab38272518a815
XiyouNiGo <1275810355@qq.com>
2022-01-28 20:39:18 +0800

    feat: add support for run

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/XiyouNiGo/MyTinyDocker
+
+go 1.13
+
+require (
+	github.com/sirupsen/logrus v1.8.1
+	github.com/urfave/cli v1.22.5
+)
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+const usage = `mytinydocker is a simple container runtime implementation model after mydocker by xianlubird.`
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "mydocker"
+	app.Usage = usage
+
+	app.Commands = []cli.Command{
+		initCommand,
+		runCommand,
+	}
+
+	app.Before = func(context *cli.Context) error {
+		log.SetFormatter(&log.JSONFormatter{})
+		log.SetOutput(os.Stdout)
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
